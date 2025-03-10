8f3c34a263aaff0c7060e57741ef4a96f0059538
grdphilip <philiprettig11@gmail.com>
2025-03-10 17:11:03 +0100

    ,uea

--- filtering/filtering.py
+++ filtering/filtering.py
@@ -102,7 +102,7 @@ class FilteringFramework:
 
 
 
-    def set_seed(seed=42):
+    def set_seed(self,seed=42):
         torch.manual_seed(seed)
         np.random.seed(seed)
         torch.cuda.manual_seed(seed)
