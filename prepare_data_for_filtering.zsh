56e0995c3f4cc4ace6d1162b572d38a1eb0fae3c
grdphilip <philiprettig11@gmail.com>
2025-03-10 10:48:19 +0100

    now

--- prepare_data_for_filtering.zsh
+++ prepare_data_for_filtering.zsh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-python process_hf_dataset.py grdphilip/elevenlabs_syndata default train customized syndata_11labs_train_manifest.json 
+python process_hf_dataset.py grdphilip/elevenlabs_syndata default train customized syndata_11labs_train_manifest.json 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
 
 
 python process_data_for_filtering.py syndata_11labs_train_manifest.json ./
