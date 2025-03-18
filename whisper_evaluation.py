d87323ee1cb46ddb8387b781680ae74fd9e24051
grdphilip <philiprettig11@gmail.com>
2025-03-18 15:24:58 +0100

    again

--- whisper_evaluation.py
+++ whisper_evaluation.py
@@ -50,7 +50,7 @@ def main(args):
 
     # prints = ["MLS", "FLEURS", "BRACARENSE", "CV", "VALIDATION"]
     
-    filenames = ["data/manifest_data/finetuning/syndata_11labs_train_manifest.json"]
+    filenames = ["data/manifest_data/finetuning/raw/syndata_11labs_train_manifest.json"]
     prints = ["TEST"]
 
     # Create dataloaders and corresponding dataframes
