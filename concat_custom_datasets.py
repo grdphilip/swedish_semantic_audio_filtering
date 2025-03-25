cdf2426636075f66f5609889890bd711e1c7f729
grdphilip <philiprettig11@gmail.com>
2025-03-25 14:38:20 +0100

    fesfs

--- concat_custom_datasets.py
+++ concat_custom_datasets.py
@@ -2,14 +2,16 @@ import json
 
 filepaths = [
     #("filtering/data/syndata_fb_train_manifest.json", "fb"),
-    ("data/manifest_data/finetuning/preprocessed/syndata_11labs_train_manifest.json", "elevenlabs"),
+    #("data/manifest_data/finetuning/preprocessed/syndata_11labs_train_manifest.json", "elevenlabs"),
     #("data/manifest_data/finetuning/preprocessed/common_voice_train_manifest.json", "common_voice"),
-    ("data/manifest_data/finetuning/preprocessed/fleurs_train_manifest.json", "fleurs"),
-    # ("data/manifest_data/finetuning/preprocessed/syndata_11labs_val_manifest.json", "elevenlabs"),
+    #("data/manifest_data/finetuning/preprocessed/fleurs_train_manifest.json", "fleurs"),
+    ("data/manifest_data/finetuning/preprocessed/syndata_11labs_val_manifest.json", "elevenlabs"),
     # ("data/manifest_data/finetuning/preprocessed/common_voice_val_manifest.json", "common_voice"),
-    # ("data/manifest_data/finetuning/preprocessed/fleurs_val_manifest.json", "fleurs")
+    ("data/manifest_data/finetuning/preprocessed/fleurs_val_manifest.json", "fleurs")
     
 ]
+
+valdation = True
 
 def concat_datasets(filepaths):
     combined_train_manifest = []
@@ -32,7 +34,10 @@ print(f"Total entries: {len(combined_data)}")
 # Save the combined data back in the same JSONL format (one JSON object per line)
 output_filepath = "data/manifest_data/finetuning/preprocessed/"
 combinations = "_".join([source for _, source in filepaths])
-output_filepath += f"combined_{combinations}_train_manifest.json"
+if valdation:
+    output_filepath += f"combined_{combinations}_val_manifest.json"
+else:
+    output_filepath += f"combined_{combinations}_train_manifest.json"
 
 with open(output_filepath, 'w', encoding='utf-8') as out_file:
     for item in combined_data:
